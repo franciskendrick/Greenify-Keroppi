9c82620f48df92cb039ede55cae27fc9b3a39821
Francis Kendrick <franciskendrick1027@gmail.com>
2024-07-13 03:44:19 +0800

    Added thumbs up and thumbs down detection

--- detection.py
+++ detection.py
@@ -17,6 +17,10 @@ def calculate_angle(point1, point2):
     return angle
 
 
+def map_value(value, from_low, from_high, to_low, to_high):
+    return (value - from_low) * (to_high - to_low) / (from_high - from_low) + to_low
+
+
 def main():
     mp_draw = mp.solutions.drawing_utils
     mp_hand = mp.solutions.hands
@@ -51,7 +55,7 @@ def main():
                     lmList.append([id, cx, cy])
                 
                 if lmList:
-                    # Landmark indices for fingers
+                    # Determine fingers' status ###################################################
                     thumb_tip = lmList[4]
                     finger_tips = [lmList[8], lmList[12], lmList[16], lmList[20]]
                     wrist = lmList[0]
@@ -75,23 +79,48 @@ def main():
                     else:
                         finger_status.insert(0, 0)  # Thumb closed
 
-                    # Determine hand's handedness
+                    # Determine hand's handedness #################################################
                     hand_label = hand_handedness.classification[0].label  # 'Left' or 'Right'
 
-                    # Determine hand's orientation
+                    # Determine hand's orientation ################################################
                     thumb_cmc = lmList[1]
                     thumb_vec = [thumb_cmc[1] - wrist[1], thumb_cmc[2] - wrist[2]]
                     pinky_vec = [pinky_mcp[1] - wrist[1], pinky_mcp[2] - wrist[2]]
                     cross_product = thumb_vec[0] * pinky_vec[1] - thumb_vec[1] * pinky_vec[0]
                     orientation = 0 if (hand_label == 'Left' and cross_product > 0) or (hand_label == 'Right' and cross_product < 0) else 1
 
-                    # ##############################################
+                    # Determine hand's angle ######################################################
+                    index_mcp = lmList[5]
+                    pinky_mcp = lmList[17]
+                    index_mcp_coords = (index_mcp[1], index_mcp[2])
+                    pinky_mcp_coords = (pinky_mcp[1], pinky_mcp[2])
+
+                    angle = calculate_angle(index_mcp_coords, pinky_mcp_coords)
+                    mapped_angle = int(map_value(angle, 0, 360, 0, 12)) * 30
+
+                    # Adjust the angle based on handedness and orientation
+                    if (hand_label == "Left" and orientation == "Palm") or (
+                            hand_label == "Right" and orientation == "Backhand"):
+                        adjusted_angle = (mapped_angle + 180) % 360
+                    else:
+                        adjusted_angle = mapped_angle
+
+                    # Detect gestures #############################################################
                     if finger_status == [0, 1, 1, 0, 0]:  # peace
                         print("peace")
                     elif finger_status[1:5] == [1, 0, 0, 1]:  # rock & roll
                         print("rock&roll")
                     elif finger_status[1:5] == [0, 1, 0, 0] and orientation == 0:  # middle finger
                         print("middle")
+
+                    elif finger_status == [1, 0, 0, 0, 0]:
+                        if adjusted_angle in [30, 60, 90]:
+                            print("thumbs up")
+                        elif adjusted_angle in [300, 270, 240]:
+                            print("thumbs down")
+                        else:
+                            print("unrecognized")
+                        
                     else:
                         print("unrecognized")
 
