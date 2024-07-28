80d132ed4ce98382b08d0f71886e96c70476efef
Francis Kendrick <franciskendrick1027@gmail.com>
2024-07-29 03:33:15 +0800

    Added eyelids animation

--- animation/animation.ino
+++ animation/animation.ino
@@ -1,43 +1,79 @@
 #include <Servo.h>
 
-float switch1Smoothed = 90; // Start at the middle position
-float switch1Prev = 90;
+float eyesSmoothed = 90;
+float eyesPrev = 90;
+
+float lidsSmoothed = 180;
+float lidsPrev = 180;
 
 Servo eye_right;
 Servo eye_left;
+Servo eyelids;
 
 void setup() {
   Serial.begin(9600);
 
   // Attach servos to pins
-  eye_right.attach(10); // Change the pin number as needed
-  eye_left.attach(11); // Change the pin number as needed
+  eyelids.attach(9);
+  eye_right.attach(10);
+  eye_left.attach(11);
 
   // Move to start position initially
   eye_right.write(90);
   eye_left.write(90);
-  delay(500); // Wait for 0.5 seconds
+  eyelids.write(90);
+
+  // Wait for 0.5 seconds
+  delay(500); 
 }
 
 void loop() {
-  smoothMoveTo(55);  // Look Left
-  smoothMoveTo(125); // Look Right
-  smoothMoveTo(90);  // Return to Start Position
+  MoveEyesTo(90);  // start
+
+  MoveLidsTo(180);  // close
+  MoveLidsTo(90);  // open
+
+  delay(100);
+
+  MoveEyesTo(55);  // left
+  MoveEyesTo(125);  // right
+  MoveEyesTo(90);  // start
+
+  MoveLidsTo(180);  // close
+  MoveLidsTo(90);  // open
+
+  delay(100);
 }
 
-void smoothMoveTo(int targetPosition) {
-  while (abs(switch1Smoothed - targetPosition) > 0.1) {
-    switch1Smoothed = (targetPosition * 0.15) + (switch1Prev * 0.85);
-    switch1Prev = switch1Smoothed;
+void MoveEyesTo(int targetPosition) {
+  while (abs(eyesSmoothed - targetPosition) > 0.1) {
+    eyesSmoothed = (targetPosition * 0.15) + (eyesPrev * 0.85);
+    eyesPrev = eyesSmoothed;
 
-    eye_right.write(int(switch1Smoothed));
-    eye_left.write(int(switch1Smoothed));
+    eye_right.write(int(eyesSmoothed));
+    eye_left.write(int(eyesSmoothed));
 
     Serial.print("Target: ");
     Serial.print(targetPosition);
     Serial.print(" , Smoothed: ");
-    Serial.println(switch1Smoothed);
+    Serial.println(eyesSmoothed);
 
-    delay(10); // Adjust the delay to control the smoothness
+    delay(10);
+  }
+}
+
+void MoveLidsTo(int targetPosition) {
+  while (abs(lidsSmoothed - targetPosition) > 0.1) {
+    lidsSmoothed = (targetPosition * 0.2) + (lidsPrev * 0.8);
+    lidsPrev = lidsSmoothed;
+
+    eyelids.write(int(lidsSmoothed));
+
+    Serial.print("Target: ");
+    Serial.print(targetPosition);
+    Serial.print(" , Smoothed: ");
+    Serial.println(lidsSmoothed);
+
+    delay(10);
   }
 }
