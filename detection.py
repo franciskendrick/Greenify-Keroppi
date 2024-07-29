a4824c44be4bc723cc5a4beb6b317e20fa8db7f9
Francis Kendrick <franciskendrick1027@gmail.com>
2024-07-30 07:01:25 +0800

    Added play audio

--- detection.py
+++ detection.py
@@ -1,8 +1,22 @@
-import cv2
+import os
 import math
+import time
+import cv2
+import random
 import mediapipe as mp
 from collections import deque
 from cvfpscalc import CvFpsCalc
+import pygame
+
+# Initialize Pygame mixer
+pygame.mixer.init()
+
+audio_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        "audio"
+    )
+)
 
 
 def calculate_distance(point1, point2):
@@ -12,7 +26,7 @@ def calculate_distance(point1, point2):
 def calculate_angle(point1, point2):
     x1, y1 = point1
     x2, y2 = point2
-    angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+    angle = math.degrees(math.atan2(y2 - y1, x2 - y1))
     if angle < 0:
         angle += 360
     return angle
@@ -20,6 +34,11 @@ def calculate_angle(point1, point2):
 
 def map_value(value, from_low, from_high, to_low, to_high):
     return (value - from_low) * (to_high - to_low) / (from_high - from_low) + to_low
+
+
+def play_sound(file_path):
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
 
 
 def main():
@@ -48,6 +67,18 @@ def main():
 
     # Dictionary to store middle finger MCP positions for wave detection for each hand
     mcp_positions_dict = {}
+
+    # Gesture names and their corresponding directories
+    gesture_names = ["Greetings", "Eyy", "2 Joints", "Rock & Roll", "Peace", "Middle Finger", "Thumbs Up", "Thumbs Down", "Waving"]
+    gesture_to_sound = {
+        gesture: [os.path.join(audio_path, f"{gesture}", file) 
+            for file in os.listdir(os.path.join(audio_path, f"{gesture}")) if file.endswith(".mp3")]
+                for gesture in gesture_names
+    }
+
+    # 
+    audio_on = False
+    deltatime = time.time()
 
     while True:
         fps = cvFpsCalc.get()
@@ -122,7 +153,7 @@ def main():
                     mapped_angle = int(map_value(angle, 0, 360, 0, 12)) * 30
 
                     # Detect gestures #############################################################
-                    gesture_detected = "unrecognized"
+                    gesture_detected = 0  # unrecognized
                     
                     index_middle_distance = calculate_distance(
                         (lmList[8][1], lmList[8][2]), 
@@ -131,34 +162,57 @@ def main():
                     waving_threshold = 150
 
                     if finger_status == [1, 0, 0, 0, 1]:  # eyy
-                        gesture_detected = 0
-                    elif finger_status == [0, 0, 0, 1, 1]:  # 2 joints
                         gesture_detected = 1
+                    elif finger_status == [0, 0, 0, 1, 1]:  # 2 joints
+                        gesture_detected = 2
 
                     elif finger_status[1:5] == [1, 0, 0, 1]:  # rock & roll
-                        gesture_detected = 2
+                        gesture_detected = 3
                     
                     elif finger_status == [0, 1, 1, 0, 0] and index_middle_distance > peacesign_threshold:  # peace
-                        gesture_detected = 3
-                    elif finger_status[1:5] == [0, 1, 0, 0] and orientation == 0:  # middle finger
                         gesture_detected = 4
+                    elif finger_status[1:5] == [0, 1, 0, 0] and orientation == 0:  # middle finger
+                        gesture_detected = 5
 
                     elif finger_status == [1, 0, 0, 0, 0]:  # thumbs up & thumbs down
                         if mapped_angle in [30, 60, 90]:
-                            gesture_detected = 5
-                        elif mapped_angle in [300, 270, 240]:
                             gesture_detected = 6
+                        elif mapped_angle in [300, 270, 240]:
+                            gesture_detected = 7
 
                     elif finger_status[1:5] == [1, 1, 1, 1] and orientation == 1:  # wave
                         mcp_positions_dict[idx].append(middle_mcp[1])
                         if len(mcp_positions_dict[idx]) == mcp_positions_dict[idx].maxlen:
                             diffs = [abs(mcp_positions_dict[idx][i] - mcp_positions_dict[idx][i + 1]) for i in range(len(mcp_positions_dict[idx]) - 1)]
                             if sum(diffs) > waving_threshold:
-                                gesture_detected = 7
+                                gesture_detected = 8
 
                     gestures.append(gesture_detected)
                 mp_draw.draw_landmarks(image, hand_landmark, mp_hand.HAND_CONNECTIONS)
-            # print(gestures)  # !!!
+            print(gestures)  # !!!
+
+            # Play Sound ##########################################################################
+            # 1 eyy
+            # 2 2joints
+            # 3 rock&roll
+            # 4 peace
+            # 5 middle finger
+            # 6 thumbs up
+            # 7 thumbs down
+            # 8 waving
+
+            currenttime = time.time()
+            sound_files = gesture_to_sound[gesture_names[gesture_detected]]
+            if len(sound_files) > 0:  # !!!
+                if not audio_on:
+                    sound_file = random.choice(sound_files)
+                    play_sound(sound_file)
+                    audio_on = True
+                    deltatime = currenttime
+                else:
+                    if not pygame.mixer.music.get_busy() and (currenttime - deltatime) >= 3:
+                        audio_on = False
+                        deltatime = currenttime
 
         cv2.putText(image, f"FPS: {fps}", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 100, 100), 2, cv2.LINE_AA)
 
@@ -169,7 +223,6 @@ def main():
 
     video.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     main()
